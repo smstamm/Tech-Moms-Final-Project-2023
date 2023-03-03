f8aa17d8cdd75539f96a993c2809c8d9584c7622
Steph <steph.m.stamm@gmail.com>
2023-03-02 19:11:42 -0700

    New files

---
+++ index.js
@@ -0,0 +1,110 @@
+// console.log('Hello, World!')
+
+// variables that change
+let banana = 'green'
+
+// variables that remain constant
+const isABananaTree = false
+
+let letterToFutureMe = 'Hi Steph,'
+letterToFutureMe = letterToFutureMe + ' you are pretty rad!'
+
+
+
+
+
+
+
+
+const ageNumber = 40
+const ageString = '40'
+
+const isItBouillon = true // could also be false
+// console.log(isItBouillon)
+
+if (isItBouillon) {
+  // console.log('Yep, it is bouillon')
+} else {
+  // console.log('Nope, not bouillon')
+}
+
+
+
+
+
+
+let capacity;
+
+
+
+
+
+let cat = {
+  name: 'Francis',
+  breed: 'tabby',
+  color: 'orange',
+  gender: 'male',
+  age: 9.5,
+  isCuddly: true,
+  favoriteFoods: ['fish', 'beans', 'bread', 'treats']
+};
+
+// console.log(cat.breed)
+// console.log(cat.color)
+
+const barGraphData = {
+  month: 'Feb',
+  day: 14,
+  amountSpent: 100,
+  percentSpent: 50,
+}
+
+barGraphData.percentSpent * 100
+
+
+
+
+
+let person = {
+  name: 'steph',
+  breed: 'mutt',
+  age: 40,
+  isCuddly: true,
+  favoriteFoods: ['ice cream', 'cheesecake', 'icing'],
+}
+
+
+const students = [
+  {
+    name: 'steph',
+    breed: 'mutt',
+    age: 40,
+    isCuddly: true,
+    favoriteFoods: ['ice cream', 'cheesecake', 'icing'],
+  },
+  {
+    name: 'Tanya'
+  },
+  {
+    name: 'Sarah'
+  },
+  {
+    name: 'Jessica'
+  }
+]
+
+// console.log('student array length', students.length);
+
+// console.log(students[students.length - 1])
+// console.log(students[3])
+
+
+
+
+
+
+
+
+
+
+
